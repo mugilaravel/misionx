da052e1a8a5e3586ce6714b5b168a2eaa7dd13d4
mugi22 <mugihnf@gmail.com>
2020-07-24 10:27:13 +0700

    httpas unutk css

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,8 +23,12 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
+        if(env('REDIRECT_HTTPS'))
+        {
+            $url->forceScheme('https');
+        }
         Schema::defaultStringLength(191);
     }
 }
